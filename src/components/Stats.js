4d2fbc30fc5e746f47599eba074ee7090f57f90a
Sudarshan Badireddi <santosh.autodesk@gmail.com>
2023-08-02 21:08:26 -0400

    added build.yml

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import './Stats.css'
 function Stats({stats,title,isCurrency}) {
+    const formatCurrency = (amount) => {
+        return new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD',
+        }).format(amount);
+    }
   return (
     <React.Fragment>
         <table className='stats-table'>
@@ -12,31 +18,31 @@ function Stats({stats,title,isCurrency}) {
     <tbody>
         <tr>
             <td>Max {isCurrency?"$ spent": "Shares purchased"}</td>
-            <td>{stats.max}</td>
+            <td>{isCurrency?formatCurrency(stats.max):stats.max}</td>
         </tr>
         <tr>
             <td>Min {isCurrency?"$ spent": "Shares purchased"}</td>
-            <td>{stats.min}</td>
+            <td>{isCurrency?formatCurrency(stats.min):stats.min}</td>
         </tr>
         <tr>
             <td>Mean {isCurrency?"$ spent": "Shares purchased"}</td>
-            <td>{stats.mean}</td>
+            <td>{isCurrency?formatCurrency(stats.mean):stats.mean}</td>
         </tr>
         <tr>
             <td>Median {isCurrency?"$ spent": "Shares purchased"}</td>
-            <td>{stats.median}</td>
+            <td>{isCurrency?formatCurrency(stats.median):stats.median}</td>
         </tr>
         <tr>
             <td>Standard Deviation</td>
-            <td>{stats.stdDev}</td>
+            <td>{isCurrency?formatCurrency(stats.stdDev):stats.stdDev}</td>
         </tr>
         <tr>
             <td>Standard Deviation - 1</td>
-            <td>{stats.stdMinus1}</td>
+            <td>{isCurrency?formatCurrency(stats.stdMinus1):stats.stdMinus1}</td>
         </tr>
         <tr>
             <td>Standard Deviation + 1</td>
-            <td>{stats.stdPlus1}</td>
+            <td>{isCurrency?formatCurrency(stats.stdPlus1):stats.stdPlus1}</td>
         </tr>
     </tbody>
 </table>
